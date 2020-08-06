e99ad206fa67bd71e1ea94bb41d736a3e81695b5
Eden <gresseden@gmail.com>
2020-08-05 22:55:33 -0400

    aplying ejs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express()
 // Settings
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
-app.engine('html', require('ejs').renderFile)
+// app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 
 //Middlewares
