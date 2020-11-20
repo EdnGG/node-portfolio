236594a71efbc3750c3be3e8a32e9b2bd590e9dc
Eden <gresseden@gmail.com>
2020-11-19 21:46:08 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 
 // Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3001)
 app.set('views', path.join(__dirname, 'views'))
 // app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
