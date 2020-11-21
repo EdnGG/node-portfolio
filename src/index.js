bb8569d15897b65f6d29d9c296be188da7450abe
Eden <gresseden@gmail.com>
2020-11-20 23:00:37 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const app = express()
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 3001)
-app.set('views', path.join(__dirname, 'views'))
+app.set("port", process.env.PORT || 3001);
+app.set("views", path.join(__dirname, "views"));
 // app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //Middlewares
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // Routes
-app.use(require('./routes/index'))
+app.use(require("./routes/index"));
 
 //Static files
-app.use(express.static(path.join(__dirname, 'public')))
-  // app.use('/static', express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use('/static', express.static('public'));
 
-
+console.log(path.join(__dirname, "public"));
 
 app.use(function (req, res, next) {
   // res.status(404).sendFile('views/404.ejs');
-  res.render('404')
-
+  res.status(404).render("404");
 });
 
 // Listening the server
-app.listen(app.get('port'), () => {
-  console.log('Server listen on', app.get('port'))
-})
+app.listen(app.get("port"), () => {
+  console.log("Server listen on", app.get("port"));
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,53 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
-router.get('/about', (req, res) => {
-  res.render('about');
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
-router.get('/fetch-api', (req, res) => {
-  res.render('fetch-api');
+router.get("/fetch-api", (req, res) => {
+  res.render("fetch-api");
 });
 
-router.get('/interactive-form', (req, res) => {
-  res.render('interactive-form');
+router.get("/interactive-form", (req, res) => {
+  res.render("interactive-form");
 });
 
-router.get('/pagination-filter', (req, res) => {
-  res.render('pagination-filter');
+router.get("/pagination-filter", (req, res) => {
+  res.render("pagination-filter");
 });
 
-router.get('/quote-gen', (req, res) => {
-  res.render('quote-gen');
+router.get("/quote-gen", (req, res) => {
+  res.render("quote-gen");
 });
 
-router.get('/tic-tac', (req, res) => {
-  res.render('tic-tac');
+router.get("/tic-tac", (req, res) => {
+  res.render("tic-tac");
 });
 
-router.get('/vsi-project', (req, res) => {
-  res.render('vsi-project');
+router.get("/vsi-project", (req, res) => {
+  res.render("vsi-project");
 });
 
-router.get('/api-vue', (req, res) => {
-  res.render('api-vue');
+router.get("/api-vue", (req, res) => {
+  res.render("api-vue");
 });
 
-router.get('/block-buster', (req, res) => {
-  res.render('block-buster');
+router.get("/block-buster", (req, res) => {
+  res.render("block-buster");
 });
 
-router.get('/vue-localstorage', (req, res) => {
-  res.render('vue-localStorage');
+router.get("/vue-localstorage", (req, res) => {
+  res.render("vue-localStorage");
 });
 
-router.get('/api-vue-firebase', (req, res) => {
-  res.render('api-auth-vue-firebase');
+router.get("/api-vue-firebase", (req, res) => {
+  res.render("api-auth-vue-firebase");
 });
 module.exports = router;
